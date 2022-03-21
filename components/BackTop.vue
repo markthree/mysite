994a7de9e694d9ea6b16c9719d26e28e100e7ecc
markthree <1801982702@qq.com>
2022-03-21 21:13:08 +0800

    chore(components): add backtop/visible, animate-bounce

--- components/BackTop.vue
+++ components/BackTop.vue
@@ -5,11 +5,19 @@
 			behavior: 'smooth'
 		})
 	}
+	const isScrollingRef = ref(false)
+	onMounted(() => {
+		const { arrivedState } = useScroll(window)
+		watchEffect(() => {
+			isScrollingRef.value = !arrivedState.top
+		})
+	})
 </script>
 
 <template>
 	<div
-		class="bg-white rounded-full cursor-pointer flex h-12 shadow-xl right-10 bottom-8 w-12 items-center justify-center fixed"
+		v-show="isScrollingRef"
+		class="bg-white rounded-full cursor-pointer flex h-12 shadow-xl right-10 bottom-8 animate-bounce w-12 items-center justify-center fixed"
 		dark="bg-gray-600 text-white"
 		@click="backTop()"
 	>
