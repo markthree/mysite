c11b237c5ee4fcceab4e9368ea152bfa1c66892d
markthree <1801982702@qq.com>
2022-05-25 18:05:48 +0800

    chore: update deps

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { defineNuxtConfig } from 'nuxt3'
+import { defineNuxtConfig } from 'nuxt'
 import presetIcons from '@unocss/preset-icons'
 import { presetAttributify, presetUno } from 'unocss'
 
