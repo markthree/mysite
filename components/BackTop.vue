fd91c651294fe5d900508baf60a9524cebdc82df
markthree <1801982702@qq.com>
2023-01-05 19:57:35 +0800

    style: more beautiful backtop icon animation

--- components/BackTop.vue
+++ components/BackTop.vue
@@ -3,12 +3,32 @@ const { backtop, visible } = useBackTop()
 </script>
 
 <template>
-	<div
-		v-show="visible"
-		@click="backtop()"
-		lg="cursor-pointer"
-		class="rounded-full flex h-10 shadow-xl right-5 bottom-5 animate-duration-250 animate-fade-in w-10 items-center justify-center fixed"
-		dark="text-white shadow-gray-600">
-		<div class="i-line-md:arrow-small-left rotate-90"></div>
-	</div>
+	<Transition name="fade" mode="out-in">
+		<div
+			v-show="visible"
+			@click="backtop()"
+			lg="cursor-pointer"
+			class="rounded-full flex h-10 shadow-xl right-5 bottom-5 w-10 items-center justify-center fixed"
+			dark="text-white shadow-gray-600">
+			<div
+				class="i-line-md:arrow-small-left rotate-90"></div>
+		</div>
+	</Transition>
 </template>
+
+<style scoped>
+.fade-enter-active,
+.fade-leave-active {
+	transition: all 0.25s ease-out;
+}
+
+.fade-enter-from {
+	opacity: 0;
+	transform: translateZ(1);
+}
+
+.fade-leave-to {
+	opacity: 0;
+	transform: translateZ(-1);
+}
+</style>
